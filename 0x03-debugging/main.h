41460a3ccfb268cba5de8760ee226fbcb44f4597
kimaita <johnnieqaym@gmail.com>
2022-12-14 07:49:26 +0000

    New project

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ *positive_or_negative - checks the sign of a number
+ *@i: the integer to check
+ *
+ */
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
