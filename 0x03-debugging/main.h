89265ae1399357d16c4c6dc044fa0f5ef9469af0
kimaita <johnnieqaym@gmail.com>
2022-12-14 08:08:38 +0000

    Add task 2 files

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,5 +8,11 @@
  *
  */
 void positive_or_negative(int i);
-
+/**
+ *largest_number - returns the largest of 3 numbers
+ *@int: the numbers to compare
+ *
+ *Return: the largest of the 3
+ */
+int largest_number(int, int, int);
 #endif /* MAIN_H */
