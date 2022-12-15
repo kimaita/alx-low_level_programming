95b8254d5a24695dd18292765a7430577e27755b
kimaita <johnnieqaym@gmail.com>
2022-12-15 20:17:21 +0000

    Fix 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,29 +6,32 @@
  *@n: integer to be printed
  *
  */
-void print_number(int n)
+void print_number(int num)
 {
 	int i;
 	bool isValid = false;
+	unsigned int n;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n == 0)
+	if (num == 0)
 		_putchar('0');
-
-	for (i = 10e8; i >= 1; i /= 10)
-	{
-		if ((n / i) != 0)
+	else{
+		if (num < 0)
 		{
-			isValid = true;
-			_putchar((n / i) + '0');
+			_putchar('-');
+			 n = -((unsigned int)num);
 		}
-		else if (isValid)
-			_putchar('0');
-		n = n % i;
+		else
+			n = num;
+		for (i = 10e8; i >= 1; i /= 10)
+		{
+			if ((n / i) != 0)
+			{
+				isValid = true;
+				_putchar((n / i) + '0');
+			}
+			else if (isValid)
+				_putchar('0');
+			n = n % i;
+		}
 	}
 }
