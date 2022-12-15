903540183e4aefed43481fd1993962f9e941d027
kimaita <johnnieqaym@gmail.com>
2022-12-15 19:55:57 +0000

    Add advanced task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdbool.h>
 
 /**
  *print_number - prints an integer using _putchar
@@ -8,6 +9,7 @@
 void print_number(int n)
 {
 	int i;
+	bool isValid = false;
 
 	n = (long) n;
 
@@ -15,11 +17,20 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n = -n;
+	}
+
+	if (n == 0)
+		_putchar('0');
 
 	for (i = 10e8; i >= 1; i /= 10)
 	{
 		if ((n / i) != 0)
+		{
+			isValid = true;
 			_putchar((n / i) + '0');
+		}
+		else if (isValid)
+			_putchar('0');
 		n = n % i;
 	}
 
