c3dcfba6127a1ff1465b0fa39ffc77962cede61d
kimaita <johnnieqaym@gmail.com>
2022-12-20 02:04:28 +0000

    Fix 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
  *puts2 - prints every other character of a string,
  *starting with the first character
@@ -8,10 +8,11 @@
  */
 void puts2(char *str)
 {
-	while (*str != 0)
-	{
-		_putchar(*str);
-		str += 2;
-	}
+	int i, l;
+
+	l = _strlen(str);
+	for (i = 0; i < l; i += 2)
+		_putchar(*(str + i));
+
 	_putchar('\n');
 }
