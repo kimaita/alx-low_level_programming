9fc1b2d417ce8f0439f640e2c52d0a3c51c6b24a
kimaita <johnnieqaym@gmail.com>
2022-12-21 15:06:05 +0000

    Add second task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncat - concatenates n bytes of a string to another
+ *@dest: pointer to string to concatenate to
+ *@src: pointer to string to concatenate
+ *@n: number of bytes to concatenate
+ *
+ *Return: pointer to concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = strlen(dest);
+	int src_len = strlen(src);
+	int i = 0;
+
+	while (i < n)
+	{
+		*(dest + dest_len + i) = *(src + i);
+		i++;
+	}
+
+	if (src_len < n)
+		*(dest + dest_len + n) = '\0';
+	return (dest);
+}
