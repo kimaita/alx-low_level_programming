3de2e0092d3663b509418c6c638c7c593deffc60
kimaita <johnnieqaym@gmail.com>
2023-01-03 14:30:21 +0000

    Add task #2

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,12 +11,33 @@
 int _putchar(char c);
 
 /**
- *_memset - function that fills memory with a constant byte
- *@s: pointer to the memory area
- *@b: constant byte to fill the memory area
- *@n: bytes of memory area to fill with b
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to the memory area
+ * @b: constant byte to fill the memory area
+ * @n: bytes of memory area to fill with b
  *
- *Return: pointer to memory area s
+ * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy  - function that copies a memory area
+ * @dest: pointer to memory area to copy to
+ * @src: pointer to memory area to copy from
+ * @n: number of bytes to copy from src to dest
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: the string to search in
+ * @c: the character to search for
+ *
+ *Return:
+ * pointer to the first occurence of c in s if present
+ * NULL if c is not  found
+ */
+char *_strchr(char *s, char c);
 #endif
