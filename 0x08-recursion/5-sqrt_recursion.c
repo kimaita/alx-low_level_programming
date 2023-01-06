b1ee9b744c38273cc19c273b216809d596b6d31a
kimaita <johnnieqaym@gmail.com>
2023-01-06 01:16:45 +0000

    Add tasks #5, #6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,26 @@
 #include "main.h"
+/**
+ * helper - helps decide if i'm right
+ * @i: integer to guess
+ * @n: integer to get root of
+ *
+ * Return: value of root
+ */
+int helper(int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = helper(i + 1, n);
+		return (j + 1);
+	}
+	return (0);
+}
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -8,7 +30,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
+	int i = 0;
 
+	if (helper(i, n) == n && n != 1)
+		return (-1);
+	return (helper(i, n));
 }
