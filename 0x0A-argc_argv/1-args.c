452ac599ad7b4983954725cbbb0760103ebff731
kimaita <johnnieqaym@gmail.com>
2023-01-09 19:20:20 +0000

    Add task #1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed
+ * @argc: number of arguments
+ * @argv: pointer to vector of arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
