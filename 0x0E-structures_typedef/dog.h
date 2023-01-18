f0edd798a971b4ddf5e4c25d9a69e0760ea7f335
kimaita <johnnieqaym@gmail.com>
2023-01-18 00:10:03 +0000

    Add new peoject

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure with members describing a dog and its owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* #ifndef DOG_H */
