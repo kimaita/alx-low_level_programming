f73a3a98afaa55277be18eeb8748f68b833688b3
kimaita <johnnieqaym@gmail.com>
2023-01-18 01:29:31 +0000

    fix 4-new_dog.c (store copy of name and owner)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,18 +11,40 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, len1, len2;
 	dog_t *ndog;
-       	char *nm;
-	char *own;
+	char *a, *b;
 
-	nm = name;
-	own = owner;
+	len1 = 0;
+	for (i = 0; name[i] != '\0'; i++)
+		len1++;
+	len2 = 0;
+	for (i = 0; owner[i] != '\0'; i++)
+		len2++;
 	ndog = malloc(sizeof(dog_t));
-
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = nm;
- 	ndog->age = age;
-	ndog->owner = own;
+	a = malloc((len1 + 1) * sizeof(char));
+	if (a == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+		a[i] = name[i];
+	a[i] = '\0';
+	ndog->name = a;
+	ndog->age = age;
+	b = malloc((len2 + 1) * sizeof(char));
+	if (b == NULL)
+	{
+		free(a);
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < len2; i++)
+		b[i] = owner[i];
+	b[i] = '\0';
+	ndog->owner = b;
 	return (ndog);
 }
