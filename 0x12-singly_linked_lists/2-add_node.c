4b9ec7be45e78cbccf1b44afb622813df885bc69
kimaita <johnnieqaym@gmail.com>
2023-01-31 13:01:41 +0000

    refactored variable

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,7 +14,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new_head == NULL)
+	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
