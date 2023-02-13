1e7819939806ea41a93265825e82991ed3119f5d
kimaita <johnnieqaym@gmail.com>
2023-02-14 00:13:29 +0300

    fix return value for NULL text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (0);
+		return (1);
 	}
 
 	write_count = write(fd, text_content, strlen(text_content));
