ea2ba58c996dccb7766482a8c94febac4eee7e49
kimaita <johnnieqaym@gmail.com>
2023-04-06 19:23:41 +0300

    checked passed size argument

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,16 +12,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
+	if (size > 0)
+	{
+		table = malloc(sizeof(hash_table_t));
+		if (table == NULL)
+			return (NULL);
 
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
-	if (table->array == NULL)
-		return (NULL);
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+		table->size = size;
+		table->array = malloc(sizeof(hash_node_t) * size);
+		if (table->array == NULL)
+		{
+			free(table);
+			return (NULL);
+		}
+		for (i = 0; i < table->size; i++)
+			table->array[i] = NULL;
 
-	return (table);
+		return (table);
+	}
+	return (NULL);
 }
